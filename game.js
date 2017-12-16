b86cf51714fa7ce3ccacf6e77687e7f29dd98a50
Noah Strayve <NoahStrayve@Noahs-MacBook-Pro-3.local>
2017-12-16 03:20:04 -0600

    jQuery perfected

--- game.js
+++ game.js
@@ -1,217 +1,184 @@
-var randomNumberOne = Math.floor(Math.random() * 12 + 1);
-var randomNumberTwo = Math.floor(Math.random() * 12 + 1);
-var randomNumberThree = Math.floor(Math.random() * 12 + 1);
-var randomNumberFour = Math.floor(Math.random() * 12 + 1);
+$(document).ready(function(){
 
-var randomNumberGoal = Math.floor(Math.random() * 102 + 19);
+	var randomNumberOne = Math.floor(Math.random() * 12 + 1);
+	var randomNumberTwo = Math.floor(Math.random() * 12 + 1);
+	var randomNumberThree = Math.floor(Math.random() * 12 + 1);
+	var randomNumberFour = Math.floor(Math.random() * 12 + 1);
 
-var countingUp = [];
-var datNumber = 0;
+	var randomNumberGoal = Math.floor(Math.random() * 102 + 19);
 
-var winner = 0;
-var loser = 0;
+	var countingUp = [];
+	var datNumber = 0;
 
-var ratio = 1;
+	var winner = 0;
+	var loser = 0;
 
-var track = [];
+	var ratio = 1;
+
+	var track = [];
 
 		
 
-document.getElementById("winCondition").innerHTML = randomNumberGoal
-document.getElementById("win").innerHTML = winner
-document.getElementById("loss").innerHTML = loser
-document.getElementById("tracker").innerHTML = track
-
-
-
-
-$("#crystalOne").on("click", function() {
-	
-	countingUp.push(randomNumberOne)
-	
-	alert("Your current total is " + datNumber)
-
-});
-		
-$("#crystalTwo").on("click", function() {
-	
-	countingUp.push(randomNumberTwo)
-	
-	alert("Your current total is " + datNumber)
-
-});
-		
-$("#crystalThree").on("click", function() {
-	
-	countingUp.push(randomNumberThree);
-	
-	alert("Your current total is " + datNumber);
-
-});
-		
-$("#crystalFour").on("click", function() {
-	
-	countingUp.push(randomNumberFour);
-	
-	alert(" Your current total is " + datNumber);
-
-});	
-
-function restartGameWin() {
-	winner = winner + ratio
+	document.getElementById("winCondition").innerHTML = randomNumberGoal
 	document.getElementById("win").innerHTML = winner
-	datNumber = 0
-	countingUp = []
-	randomNumberOne = Math.floor(Math.random() * 12 + 1);
-	randomNumberTwo = Math.floor(Math.random() * 12 + 1);
-	randomNumberThree = Math.floor(Math.random() * 12 + 1);
-	randomNumberFour = Math.floor(Math.random() * 12 + 1);
-	randomNumberGoal = Math.floor(Math.random() * 102 + 19);
-	document.getElementById("winCondition").innerHTML = randomNumberGoal
-}
+	document.getElementById("loss").innerHTML = loser
 
-function restartGameLose() {
-	loser = loser + ratio
-	document.getElementById("loss").innerHTML = loser	
-	datNumber = 0
-	countingUp = []
-	randomNumberOne = Math.floor(Math.random() * 12 + 1);
-	randomNumberTwo = Math.floor(Math.random() * 12 + 1);
-	randomNumberThree = Math.floor(Math.random() * 12 + 1);
-	randomNumberFour = Math.floor(Math.random() * 12 + 1);
-	randomNumberGoal = Math.floor(Math.random() * 102 + 19);
-	document.getElementById("winCondition").innerHTML = randomNumberGoal
-}
-
-function numberOne() {
-	
-	alert(randomNumberOne)
-
-	countingUp = []
-			
-	countingUp.push(randomNumberOne)
-			
-	for (var i = 0; i< countingUp.length; i++) {
+	$(".crystalOne").on("click", function() {
 		
-		datNumber += countingUp[i];
+		alert(randomNumberOne)
+
+		countingUp = []
+			
+		countingUp.push(randomNumberOne)
+			
+		for (var i = 0; i< countingUp.length; i++) {
+		
+			datNumber += countingUp[i];
 	
+		}
+	
+		alert("Your have " + datNumber + " gems!")
+			
+		if (datNumber === randomNumberGoal) {
+		
+			alert("YOU WIN!!!!!!!")
+		
+			restartGameWin()
+	
+		}
+
+		else if (datNumber > randomNumberGoal) {
+		
+			alert("YOU LOSE!!!!!!!")
+		
+			restartGameLose()
+	
+		}
+	});
+		
+	$(".crystalTwo").on("click", function() {
+	
+		alert(randomNumberTwo)
+
+		countingUp = []
+			
+		countingUp.push(randomNumberTwo)
+			
+		for (var i = 0; i< countingUp.length; i++) {
+		
+			datNumber += countingUp[i];
+	
+		}
+	
+		alert("Your have " + datNumber + " gems!")
+			
+		if (datNumber === randomNumberGoal) {
+		
+			alert("YOU WIN!!!!!!!")
+		
+			restartGameWin()
+	
+		}
+
+		else if (datNumber > randomNumberGoal) {
+		
+			alert("YOU LOSE!!!!!!!")
+		
+			restartGameLose()
+	
+		}
+	});
+		
+	$(".crystalThree").on("click", function() {
+	
+		alert(randomNumberThree)
+
+		countingUp = []
+			
+		countingUp.push(randomNumberThree)
+			
+		for (var i = 0; i< countingUp.length; i++) {
+		
+			datNumber += countingUp[i];
+	
+		}
+			
+		alert("Your have " + datNumber + " gems!")
+			
+		if (datNumber === randomNumberGoal) {
+		
+			alert("YOU WIN!!!!!!!")
+		
+			restartGameWin()
+	
+		}
+
+		else if (datNumber > randomNumberGoal) {
+		
+			alert("YOU LOSE!!!!!!!")
+		
+			restartGameLose()
+	
+		} 
+	});
+		
+	$(".crystalFour").on("click", function() {
+	
+		alert(randomNumberFour)
+
+		countingUp = []
+			
+		countingUp.push(randomNumberFour)
+			
+		for (var i = 0; i< countingUp.length; i++) {
+		
+			datNumber += countingUp[i];
+	
+		}
+			
+		alert("Your have " + datNumber + " gems!")
+			
+		if (datNumber === randomNumberGoal) {
+		
+			alert("YOU WIN!!!!!!!")
+		
+			restartGameWin()
+	
+		}
+
+		else if (datNumber > randomNumberGoal) {
+		
+			alert("YOU LOSE!!!!!!!")
+		
+			restartGameLose()
+	
+		} 
+	});	
+
+	function restartGameWin() {
+		winner++
+		document.getElementById("win").innerHTML = winner
+		datNumber = 0
+		countingUp = []
+		randomNumberOne = Math.floor(Math.random() * 12 + 1);
+		randomNumberTwo = Math.floor(Math.random() * 12 + 1);
+		randomNumberThree = Math.floor(Math.random() * 12 + 1);
+		randomNumberFour = Math.floor(Math.random() * 12 + 1);
+		randomNumberGoal = Math.floor(Math.random() * 102 + 19);
+		document.getElementById("winCondition").innerHTML = randomNumberGoal
+	}	
+
+	function restartGameLose() {
+		loser = loser + ratio
+		document.getElementById("loss").innerHTML = loser	
+		datNumber = 0
+		countingUp = []
+		randomNumberOne = Math.floor(Math.random() * 12 + 1);
+		randomNumberTwo = Math.floor(Math.random() * 12 + 1);
+		randomNumberThree = Math.floor(Math.random() * 12 + 1);
+		randomNumberFour = Math.floor(Math.random() * 12 + 1);
+		randomNumberGoal = Math.floor(Math.random() * 102 + 19);
+		document.getElementById("winCondition").innerHTML = randomNumberGoal
 	}
-			
-	alert("Your have " + datNumber + " gems!")
-			
-	if (datNumber === randomNumberGoal) {
-		
-		alert("YOU WIN!!!!!!!")
-		
-		restartGameWin()
-	
-	}
 
-	else if (datNumber > randomNumberGoal) {
-		
-		alert("YOU LOSE!!!!!!!")
-		
-		restartGameLose()
-	
-	} 
-}
-
-function numberTwo() {
-			
-	alert(randomNumberTwo)
-
-	countingUp = []
-			
-	countingUp.push(randomNumberTwo)
-			
-	for (var i = 0; i< countingUp.length; i++) {
-		
-		datNumber += countingUp[i];
-	
-	}
-	
-	alert("Your have " + datNumber + " gems!")
-			
-	if (datNumber === randomNumberGoal) {
-		
-		alert("YOU WIN!!!!!!!")
-		
-		restartGameWin()
-	
-	}
-
-	else if (datNumber > randomNumberGoal) {
-		
-		alert("YOU LOSE!!!!!!!")
-		
-		restartGameLose()
-	
-	} 
-}
-
-function numberThree() {
-			
-	alert(randomNumberThree)
-
-	countingUp = []
-			
-	countingUp.push(randomNumberThree)
-			
-	for (var i = 0; i< countingUp.length; i++) {
-		
-		datNumber += countingUp[i];
-	
-	}
-			
-	alert("Your have " + datNumber + " gems!")
-			
-	if (datNumber === randomNumberGoal) {
-		
-		alert("YOU WIN!!!!!!!")
-		
-		restartGameWin()
-	
-	}
-
-	else if (datNumber > randomNumberGoal) {
-		
-		alert("YOU LOSE!!!!!!!")
-		
-		restartGameLose()
-	
-	} 
-}
-
-function numberFour() {
-			
-	alert(randomNumberFour)
-
-	countingUp = []
-			
-	countingUp.push(randomNumberFour)
-			
-	for (var i = 0; i< countingUp.length; i++) {
-		
-		datNumber += countingUp[i];
-	
-	}
-			
-	alert("Your have " + datNumber + " gems!")
-			
-	if (datNumber === randomNumberGoal) {
-		
-		alert("YOU WIN!!!!!!!")
-		
-		restartGameWin()
-	
-	}
-
-	else if (datNumber > randomNumberGoal) {
-		
-		alert("YOU LOSE!!!!!!!")
-		
-		restartGameLose()
-	
-	} 
-
-}
+});
