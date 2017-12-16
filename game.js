ec468be95a45e8a5f8198bc3816f8a407e5f86fb
Noah Strayve <NoahStrayve@Noahs-MacBook-Pro-3.local>
2017-12-15 19:16:11 -0600

    alert buttons working

--- game.js
+++ game.js
@@ -1,25 +1,24 @@
-generateNumbers: function(){
-	this.randomNum = Math.floor((Math.random() * 120) + 19);
-	this.buttonANum = Math.floor((Math.random() * 12) + 1);
-	this.buttonBNum = Math.floor((Math.random() * 12) + 1);
-	this.buttonCNum = Math.floor((Math.random() * 12) + 1);
-	this.buttonDNum = Math.floor((Math.random() * 12) + 1);
-},
-
-printGameStats: function(){
-	console.log("Random number: " + this.randomNum);
-	console.log("A number: " + this.buttonANum);
-	console.log("B number: " + this.buttonBNum);
-	console.log("C number: " + this.buttonCNum);
-	console.log("D number: " + this.buttonDNum);
-}
-
 var winCounter = 0;
 var lossCounter = 0;
 var finalNumber = ""
 var currentNumber = ""
-var gemOne
-var gemTwo
-var gemThree
-var gemFour
+var gemOne =
+var gemTwo = 
+var gemThree =  
+var gemFour =  
 
+function numberOne() {
+	alert("Button WORKS!!!")
+}
+
+function numberTwo() {
+	alert("Button WORKS!!!")
+}
+
+function numberThree() {
+	alert("Button WORKS!!!")
+}
+
+function numberFour() {
+	alert("Button WORKS!!!")
+}
